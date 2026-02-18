7aebb03ef18f2e7b91a20853e67d6c085855d249
CarlimaUNS <144245079+CarlimaUNS@users.noreply.github.com>
2026-02-18 18:36:55 -0300

    Submit implementado correctamente

--- ngproyect/ngproyectvite/src/App.jsx
+++ ngproyect/ngproyectvite/src/App.jsx
@@ -1,8 +1,11 @@
 import Workers from "./comp/Workers";
+import Header from "./comp/Header";
+import"./App.css";
 //Componente aplicacion inicial
 function App() {
   return (
     <div className="App">
+      <Header/>
       {/*Componente principal que se carga dentro de la app*/}
       <Workers/>
     </div>
